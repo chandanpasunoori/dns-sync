f978d72d8131661a8731b6b8ce8027778205106d
Chandan Pasunoori <chandanpasunoori@gmail.com>
2021-10-01 03:09:53 +0530

    filtering from ignore list and health checking before updating dns records

--- pkg/jobs.go
+++ pkg/jobs.go
@@ -6,20 +6,38 @@ type Source struct {
 	Interval Duration `json:"interval"`
 }
 type Destination struct {
-	Type       string   `json:"type"`
-	Project    string   `json:"project"`
-	Zone       string   `json:"zone"`
-	ZoneName   string   `json:"zoneName"`
-	RecordName string   `json:"recordName"`
-	RecordType string   `json:"recordType"`
-	IpType     string   `json:"ipType"`
-	Ttl        Duration `json:"ttl"`
+	Type               string         `json:"type"`
+	Project            string         `json:"project"`
+	Zone               string         `json:"zone"`
+	ZoneName           string         `json:"zoneName"`
+	RecordName         string         `json:"recordName"`
+	RecordType         string         `json:"recordType"`
+	IpType             string         `json:"ipType"`
+	Ttl                Duration       `json:"ttl"`
+	ReadinessProbe     ReadinessProbe `json:"readinessProbe,omitempty"`
+	IgnoreListFilePath string         `json:"ignoreListFilePath,omitempty"`
 }
 
 type Filter struct {
 	LabelKey   string `json:"labelKey"`
 	LabelValue string `json:"labelValue"`
 	Type       string `json:"type"`
+}
+
+type HTTPGet struct {
+	Path   string `json:"path"`
+	Port   int    `json:"port"`
+	Scheme string `json:"scheme"`
+}
+
+type ReadinessProbe struct {
+	Period           Duration `json:"period"`
+	Timeout          Duration `json:"timeout"`
+	SuccessThreshold int      `json:"successThreshold"`
+	FailureThreshold int      `json:"failureThreshold"`
+	IpType           string   `json:"ipType"`
+	HTTPGet          HTTPGet  `json:"httpGet,omitempty"`
+	ProgressDeadline Duration `json:"progressDeadline"`
 }
 type Job struct {
 	Name    string `json:"name"`
@@ -40,5 +58,9 @@ type Node struct {
 }
 
 type Event struct {
+	Nodes []Node `json:"nodes"`
+}
+
+type IgnoredNodes struct {
 	Nodes []Node `json:"nodes"`
 }
